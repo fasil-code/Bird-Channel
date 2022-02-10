ee827a3f76cb0e903eb935628750ca5c2686cd7e
fasil shafi <zargerfasil123@gmail.com>
2022-02-09 23:13:08 -0800

    add

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import Birding from './Containers/Birding/Birding';
 import Homek from './Containers/Home/Home';
-
+import Contactm from './Containers/Contactm/Contactm';
 function App() {
   return (
     <div className="App">
    {/* <Homek /> */}
-   <Birding/>
+   {/* <Birding/> */}
+  <Contactm />
     </div>
   );
 }
---
+++ src/Containers/Contactm/Contactm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+import Contact from '../../components/ContactUs/Contact'
+function Contactm() {
+  return (
+      <div>
+<Navbar/>
+<Contact />
+<Footer />
+</div>
+  )
+}
+
+export default Contactm
