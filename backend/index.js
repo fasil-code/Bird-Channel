fc5ee77c843ecc095d53804c0a2105e4904473c8
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2022-02-14 05:02:37 +0530

    file upload system added

--- backend/index.js
+++ backend/index.js
@@ -1,74 +1,84 @@
 var mysql = require('mysql2');
- const express = require('express')
- const app = express();
- const port = 3001
- const cors=require('cors');
-const bodyParser=require('body-parser');
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(cors());
+const fileUpload = require('express-fileupload');
+const morgan = require('morgan');
+const _ = require('lodash');
+
 app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+const port = 3001
+
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "toor",
-  database:"birdchannel"
+    host: "localhost",
+    user: "root",
+    password: "toor",
+    database:"birdchannel"
 });
+
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+    if (err) throw err;
+    console.log("Connected!");
 });
 
 
-    // var sql = "SELECT * FROM birdsall";
-    // con.query(sql, function (err, result, fields) {
-    //   if (err) throw err;
-    // console.log(result);
-    // });
+app.post('/admin', async (req, res) => {
+    try {
+        if(!req.files.b_image) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            const b_name = req.body.b_name;
+            const b_cname = req.body.b_cname;
+            const b_food = req.body.b_food;
+            const b_prey = req.body.b_prey;
+            const b_habitat = req.body.b_habitat;
+            const b_desc = req.body.b_desc;
+            const b_categ = req.body.b_categ;
+            const b_image = req.files.b_image;
+            const b_image_path = './uploads/birds/' + b_image.name;
+            b_image.mv(b_image_path);
 
-  app.post('/admin',(req,res)=>{
-  const b_name = req.body.b_name;
-  const b_cname = req.body.b_cname;
-  const b_food = req.body.b_food;
-  const b_prey = req.body.b_prey;
-  const b_habitat = req.body.b_habitat;
-  const b_desc = req.body.b_desc;
-  const b_image = req.body.b_image;
-  const b_categ = req.body.b_categ;
-
-con.query(
-  "INSERT INTO birds (b_name, b_cname, b_food, b_prey, b_habitat, b_desc, b_image, b_categ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-[b_name, b_cname, b_food, b_prey, b_habitat, b_desc, b_image, b_categ],
-(err,result)=>{
-  if(err)console.log(err);
-  else{
-    res.send("Values inserted");
-  }
-}
-);
-     
- 
-  });
-
-
-  app.get('/getcategories',(req,res)=>{
-
-
-var sql = "SELECT c_id, c_name FROM categories;";
-  con.query(sql, function (err, result, fields) {
-    if (err) throw err;
-    res.send(result);
-  });
-     
- 
-  });
+            const sql = "INSERT INTO birds (b_name, b_cname, b_food, b_prey, b_habitat, b_desc, b_image, b_categ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            con.query(sql, [b_name, b_cname, b_food, b_prey, b_habitat, b_desc, b_image_path, b_categ], (err, result) => {
+                if(err)console.log(err);
+                else {
+                    res.send({
+                        status: true,
+                        message: 'Values inserted/file uploaded',
+                    });
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 
+app.get('/getcategories', (req, res) => {
+    const sql = "SELECT c_id, c_name FROM categories;";
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 
 
-
-
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+    console.log(`App is listening on PORT ${port}`)
+});
