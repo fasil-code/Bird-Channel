2cefc9e680f69f836d381afa1ba8e6dff6db457b
Jameel Kaisar <65322473+JameelKaisar@users.noreply.github.com>
2023-08-01 18:04:22 +0000

    Fix things

--- backend/create_database.js
+++ backend/create_database.js
@@ -2,7 +2,7 @@ var mysql = require('mysql2');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Zargar@123"
+  password: "password"
 });
 
 con.connect(function(err) {
